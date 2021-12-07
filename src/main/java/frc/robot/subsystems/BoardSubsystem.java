d19324de9df623e4e5e093e68262342d24fe82b2
MrMiu <38479314+MrMiu@users.noreply.github.com>
2021-12-06 19:35:19 -0500

    Updated for New Board Command

--- src/main/java/frc/robot/subsystems/BoardSubsystem.java
+++ src/main/java/frc/robot/subsystems/BoardSubsystem.java
@@ -3,31 +3,24 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class BoardSubsystem implements Subsystem {
   
   CANSparkMax motor;
   DigitalInput limitSwitch;
-  Ultrasonic ultrasonic;
-  Solenoid pneumaticsDevice;
 
   /** Creates a new Subsystem. */
   public BoardSubsystem() {
     this.motor = motor;
     this.limitSwitch = limitSwitch;
-    this.ultrasonic = ultrasonic;
-
-    Ultrasonic.setAutomaticMode(true);
   }
 
   public CANSparkMax getMotor() {
     return motor;
   }
 
-  public void setMotor(double val) { 
+  public setMotor(double val) { 
     motor.set(val);
   }
 
@@ -37,14 +30,6 @@ public class BoardSubsystem implements Subsystem {
 
   public boolean getSwitchValue() {
     return limitSwitch.get();
-  }
-
-  public Ultrasonic getUltrasonic() {
-    return ultrasonic;
-  }
-
-  public double getUltrasonicValue() {
-    return ultrasonic.getRangeInches();
   }
 
   @Override
