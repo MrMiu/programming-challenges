2d2ddda414ff0e35e2aa9fa632ee8035d8753a45
meghan-ng <meghan.ng27@gmail.com>
2021-12-06 22:47:47 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,6 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.commands.BoardCommand;
 import frc.robot.subsystems.BoardSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
