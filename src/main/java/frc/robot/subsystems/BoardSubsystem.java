d527ed206757ab64ca911b5180d9916f18acb523
meghan-ng <meghan.ng27@gmail.com>
2022-01-14 12:15:42 -0500

    Update BoardSubsystem.java
    Didn't save properly the first time, updated subsystem code.

--- src/main/java/frc/robot/subsystems/BoardSubsystem.java
+++ src/main/java/frc/robot/subsystems/BoardSubsystem.java
@@ -1,27 +1,36 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANDigitalInput.LimitSwitch;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class BoardSubsystem implements Subsystem {
   
-  CANSparkMax motor;
+  CANSparkMax motor1;
+  CANSparkMax motor2;
   DigitalInput limitSwitch;
 
-  public BoardSubsystem(CANSparkMax motor, DigitalInput limitSwitch) {
-    this.motor = motor;
+  public BoardSubsystem(CANSparkMax motor1, CANSparkMax motor2, DigitalInput limitSwitch) {
+    this.motor1 = motor1;
+    this.motor2 = motor2;
     this.limitSwitch = limitSwitch;
   }
 
-  public CANSparkMax getMotor() {
-    return motor;
+  public CANSparkMax getMotor1() {
+    return motor1;
   }
 
-  public void setMotor(double val) { 
-    motor.set(val);
+  public void setMotor1(double val) { 
+    motor1.set(val);
+  }
+
+  public CANSparkMax getMotor2() {
+    return motor2;
+  }
+
+  public void setMotor2(double val) { 
+    motor2.set(val);
   }
 
   public DigitalInput getLimitSwitch() {
